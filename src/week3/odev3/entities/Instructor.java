7015a924053af7971c1a3d5d5506cc1d54d76e73
kahveci <88493840+khvci@users.noreply.github.com>
2022-10-11 01:56:50 +0300

    remove unused class

--- src/week3/odev3/entities/Instructor.java
+++
@@ -1,9 +0,0 @@
-package week3.odev3.entities;
-
-public class Instructor {
-    private final String instructorName;
-
-    public Instructor(String instructorName) {
-        this.instructorName = instructorName;
-    }
-}
